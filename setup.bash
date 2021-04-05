92b098425106635600c4bdba5b252ac19e28d4bb
Aidan Do <43259657+REslim30@users.noreply.github.com>
2021-04-05 14:25:56 +0930

    Create setup.bash

---
+++ setup.bash
@@ -0,0 +1,8 @@
+# Script to be run only once
+#cache user and pass
+git config --global credential.helper cache 1800
+git config --global user.name "Aidan Do"
+git config --global user.email "giahuydo99@gmail.com"
+
+#aliases
+git config --global alias.cam "commit -am"
